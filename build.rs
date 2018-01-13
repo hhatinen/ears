ce8c31b3863654def8ff04b17b0044398e26cd08
Harri Hatinen <hhatinen@gmail.com>
2018-01-13 21:32:30 +0200

    add libraries for msvc compilation

--- build.rs
+++ build.rs
@@ -4,16 +4,21 @@ fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/lib");
 }
 
-#[cfg(all(windows, target_arch = "x86"))]
+#[cfg(all(windows, target_arch = "x86", target_env="gnu"))]
 fn main () {
-    println!("cargo:rustc-link-search=native=.");
     println!("cargo:rustc-link-search=native=C:\\msys32\\mingw64\\lib");
 }
 
-#[cfg(all(windows, target_arch = "x86_64"))]
+#[cfg(all(windows, target_arch = "x86_64", target_env="gnu"))]
 fn main () {
     println!("cargo:rustc-link-search=native=C:\\msys64\\mingw64\\lib");
 }
+
+#[cfg(all(windows, target_env="msvc"))]
+fn main () {
+    println!("cargo:rustc-link-search=native=.\\lib");
+}
+
 
 #[cfg(all(not(windows), not(unix)))]
 fn main() {}
